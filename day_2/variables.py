301035e8f9fbd69552b1e7e650adce9362ff7a3e
c4dburry <keyser.mathis@gmail.com>
2024-03-19 20:08:49 +0100

    python_exercises_day2

---
+++ day_2/variables.py
@@ -0,0 +1,20 @@
+#Day 2: 30 Days of python programming
+firstname = "givenname"
+lastname = "surname"
+fullname = firstname + " " + lastname
+country = "germany"
+age = 24
+year =2024
+is_married = False
+is_true = False
+is_light_on = True
+recht, links, oben, unten = 'r','l','o','u'
+
+print(type(is_light_on))
+print(len(firstname))
+
+print(len(firstname)==len(lastname))
+
+
+
+print(fullname)
